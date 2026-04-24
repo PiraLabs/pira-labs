a812f9082bc3560bd1ffb22576e15d9cb7cf3329
Celso Gama <celso.gama@piralabs.com.br>
2026-04-24 16:19:45 -0300

    revert: remover redirect www do next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,17 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.piralabs.com.br' }],
-        destination: 'https://piralabs.com.br/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
